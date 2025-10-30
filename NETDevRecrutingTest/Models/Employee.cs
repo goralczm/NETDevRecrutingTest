b63cb969fd0c7f8b9e60220d30388e4c17129887
Maciej Góralczyk <goralczm@gmail.com>
2025-10-31 00:45:16 +0100

    Add project files.

---
+++ NETDevRecrutingTest/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace NETDevRecrutingTest.Models
+{
+    internal class Employee
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public int? SuperiorId { get; set; }
+        public virtual Employee Superior { get; set; }
+    }
+}
