2115084fa45c386bc318893c48cae63577e70f03
Maciej Góralczyk <goralczm@gmail.com>
2025-11-03 06:36:52 +0100

    Add: employee structure and hierarchy

---
+++ NETDevRecrutingTest/Models/EmployeeStructure.cs
@@ -0,0 +1,9 @@
+﻿namespace NETDevRecrutingTest.Models
+{
+    internal class EmployeeStructure
+    {
+        public int EmployeeId { get; set; }
+        public int SuperiorId { get; set; }
+        public int Level { get; set; }
+    }
+}
---
+++ NETDevRecrutingTest/Services/EmployeeHierarchyService.cs
@@ -0,0 +1,46 @@
+﻿using NETDevRecrutingTest.Models;
+
+namespace NETDevRecrutingTest.Services
+{
+    internal class EmployeeHierarchyService
+    {
+        private List<EmployeeStructure> _employeesHierarchyCache = new();
+
+        public List<EmployeeStructure> FillEmployeesStructure(List<Employee> employees)
+        {
+            List<EmployeeStructure> structures = new();
+
+            foreach (Employee employee in employees)
+            {
+                if (employee.Superior == null)
+                    continue;
+
+                Employee current = employee.Superior;
+                int level = 1;
+
+                while (current != null)
+                {
+                    structures.Add(new()
+                    {
+                        EmployeeId = employee.Id,
+                        SuperiorId = current.Id,
+                        Level = level,
+                    });
+
+                    current = current.Superior;
+                    level++;
+                }
+            }
+
+            _employeesHierarchyCache = structures;
+            return structures;
+        }
+
+        public int? GetSuperiorRowOfEmployee(int employeeId, int superiorId)
+        {
+            return _employeesHierarchyCache
+                .FirstOrDefault(es => es.EmployeeId == employeeId && es.SuperiorId == superiorId)
+                ?.Level;
+        }
+    }
+}
